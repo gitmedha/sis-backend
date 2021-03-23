319a352fd89fa3a69aa95cbad21d28e017bdc5e8
Swapneel Rane <swapneelrane@gmail.com>
2021-03-23 10:04:46 +0530

    add custom route & handler for processing webhooks

--- api/employers/controllers/employers.js
+++ api/employers/controllers/employers.js
@@ -1,8 +1,27 @@
 'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    // CREATE FROM WEBHOOK
+    async createFromWebhook (ctx) {
+        
+        console.log('ctx.request.body: ',  ctx.request.body)
+
+        
+
+        let entity = await strapi.services.employers.create(ctx.request.body)
+
+        let sanitizedEntity = sanitizeEntity(entity, { model: strapi.models.employers })
+
+        console.log("Employer Created: ", sanitizedEntity)
+
+        ctx.send(sanitizedEntity)
+
+        return sanitizedEntity
+    }
+};
