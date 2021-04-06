3d1fece2667f1b6cf5fe0a6bf3e39b54d31690ba
Swapneel Rane <swapneelrane@gmail.com>
2021-04-06 16:02:02 +0530

    Updated Strapi

--- api/employers/controllers/employers.js
+++ api/employers/controllers/employers.js
@@ -9,19 +9,39 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 module.exports = {
     // CREATE FROM WEBHOOK
     async createFromWebhook (ctx) {
-        
         console.log('ctx.request.body: ',  ctx.request.body)
 
-        
+        const employer = {}
+        employer.name = ctx.request.body.name
 
-        let entity = await strapi.services.employers.create(ctx.request.body)
-
+        let entity = await strapi.services.employers.create(employer)
         let sanitizedEntity = sanitizeEntity(entity, { model: strapi.models.employers })
+        console.log(`
+            Employer Created! ID: ${sanitizedEntity.id}
+            NAME: ${sanitizedEntity.name}
+            CREATED AT: ${sanitizedEntity.created_at}
+            `)
 
-        console.log("Employer Created: ", sanitizedEntity)
+        const data = ctx.request.body        
 
+        Object.keys(data).forEach(async key => {
+            // console.log(key, ": " ,data[key])
+
+            const surveyResponse = {}
+            surveyResponse.survey_response_id = sanitizedEntity.id
+            surveyResponse.question = key
+            surveyResponse.response = data[key]
+
+            // console.log("surveyResponse: ", surveyResponse)
+
+            let unpivotedSurveyResponse = await strapi.services.surveys.create(surveyResponse)
+            console.log("unpivotedSurveyResponse: ", unpivotedSurveyResponse)
+            // return unpivotedSurveyResponse
+
+        })
+
+        
         ctx.send(sanitizedEntity)
-
         return sanitizedEntity
     }
 };
