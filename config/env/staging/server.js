94d0a8bcf33586e33be7476ff504bf12e28cd689
Swapneel Rane <swapneelrane@gmail.com>
2021-07-09 21:04:35 +0530

    add config & update strapi & meilisearch

---
+++ config/env/staging/server.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: 'https://sis-staging.eastus.cloudapp.azure.com/api',
+    admin: {
+      url: 'https://sis-staging.eastus.cloudapp.azure.com/strapi-admin',
+      auth: {
+        secret: env('ADMIN_JWT_SECRET', '5d062827bca2413203eb73dfa175efa6'),
+      },
+    },
+  });
+  
