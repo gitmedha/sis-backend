d6908f3c83d039a96f5ca1fa4c724bd71e13eb54
swapneelrane <swapneelrane@gmail.com>
2021-11-08 08:09:19 +0000

    pehle page integration

--- api/students/controllers/students.js
+++ api/students/controllers/students.js
@@ -12,60 +12,70 @@ module.exports = {
         const data = ctx.request.body
 
         const student = {}
-        student.first_name = data.firstname
-        student.last_name = data.lastname
+        student.full_name = data.full_name
         student.email = data.email
         student.phone = data.phone
         student.status = 'New Request' // default status is Registered
-        // student.date_of_birth = NOT_FOUND
-        // student.name_of_parent_or_guardian = NOT_FOUND
-        // student.category = NOT_FOUND
-        // student.gender = NOT_FOUND
+	student.date_of_birth = data.date_of_birth
+        student.name_of_parent_or_guardian = data.parent_or_guardian_name
+        student.category = data.category
+        student.gender = data.gender
+	student.assigned_to = 353
         // student.old_sis_id = NOT_FOUND
-        // student.income_level = NOT_FOUND
-        // student.cv = NOT_FOUND
+        student.income_level = data.income_level
+	// student.cv = NOT_FOUND
         // student.logo = NOT_FOUND
-        // student.registration_date_latest = NOT_FOUND
+	// student.registration_date_latest = NOT_FOUND
         // student.certification_date_latest = NOT_FOUND
         // student.internship_date_latest = NOT_FOUND
         // student.placement_date_latest = NOT_FOUND
         // student.course_type_latest = NOT_FOUND
         student.medha_champion = false
         student.interested_in_employment_opportunities = true
-        student.address = `${data.address1} ${data.address2}`
+        student.address = data.address
         student.city = data.city
-        student.pin_code = data.zipcode
-        // student.state = data.zipcode
-        // student.medha_area = data.zipcode
-        // student.assigned_to = NOT_FOUND
+        student.pin_code = data.pin_code
+        student.state = data.state
+	student.district = data.district
+	student.medha_area = data.area    
 
         let studentEntity = await strapi.services.students.create(student)
         let sanitizedStudentEntity = sanitizeEntity(studentEntity, { model: strapi.models.students })
         console.log(`
             STUDENT CREATED ID: ${sanitizedStudentEntity.id}
-            NAME: ${sanitizedStudentEntity.first_name}
+            NAME: ${sanitizedStudentEntity.full_name}
             CREATED AT: ${sanitizedStudentEntity.created_at}
         `)
 
-        const institution = await strapi.services.institutions.findOne({ id: data.batch_id });
-        const batch = await strapi.services.batches.findOne({ id: data.institution_id });
+       // const batch = await strapi.services.batches.findOne({ id: data.batch_id });
+        const institution = await strapi.services.institutions.findOne({ id: data.institution_id });
+	const program = await strapi.services.programs.findOne({ id:data.program_id });   
+        const money_id = data.payuMoneyId
 
         // create a program enrollment for the student
         const programEnrollment = {}
         programEnrollment.student = sanitizedStudentEntity
-        programEnrollment.batch = batch
+        //programEnrollment.batch = batch
         programEnrollment.institution = institution
+	programEnrollment.batch = program.default_enrollment_batch
         programEnrollment.status = 'Enrollment Request Received' // default status is Enrolled
-        // programEnrollment.registration_date = NOT_FOUND
+        programEnrollment.registration_date = new Date()
+	
+	    if(money_id == 0){
+              programEnrollment.fee_status ='Waived off'
+	    }   
+	    else{
+  	      programEnrollment.fee_status='Paid'
+	    }
         // programEnrollment.fee_status = NOT_FOUND
-        programEnrollment.fee_payment_date = data.addedon
-        programEnrollment.fee_amount = data.net_amount_debit
-        programEnrollment.fee_transaction_id = data.txnid
+        // programEnrollment.fee_payment_date = data.addedon
+        programEnrollment.fee_amount = data.amount
+        programEnrollment.fee_transaction_id = data.fee_transaction_id
         // programEnrollment.fee_refund_status = NOT_FOUND
         // programEnrollment.fee_refund_date = NOT_FOUND
-        // programEnrollment.course_type = NOT_FOUND
-        // programEnrollment.course_level = NOT_FOUND
-        // programEnrollment.course_year = NOT_FOUND
+        programEnrollment.course_type = data.course_type
+        programEnrollment.course_level = data.course_level
+        programEnrollment.course_year = data.course_year
         // programEnrollment.year_of_course_completion = NOT_FOUND
         // programEnrollment.certification_date = NOT_FOUND
         // programEnrollment.institution_name_entered_by_student = NOT_FOUND
