d652f05421689056267240ec65b76a922f192f05
swapneelrane <swapneelrane@gmail.com>
2021-11-09 03:39:35 +0000

    remove code

--- api/students/controllers/students.js
+++ api/students/controllers/students.js
@@ -21,15 +21,7 @@ module.exports = {
         student.category = data.category
         student.gender = data.gender
         student.assigned_to = 353
-        // student.old_sis_id = NOT_FOUND
         student.income_level = data.income_level
-        // student.cv = NOT_FOUND
-        // student.logo = NOT_FOUND
-        // student.registration_date_latest = NOT_FOUND
-        // student.certification_date_latest = NOT_FOUND
-        // student.internship_date_latest = NOT_FOUND
-        // student.placement_date_latest = NOT_FOUND
-        // student.course_type_latest = NOT_FOUND
         student.medha_champion = false
         student.interested_in_employment_opportunities = true
         student.address = data.address
@@ -47,7 +39,6 @@ module.exports = {
             CREATED AT: ${sanitizedStudentEntity.created_at}
         `)
 
-       // const batch = await strapi.services.batches.findOne({ id: data.batch_id });
         const institution = await strapi.services.institutions.findOne({ id: data.institution_id });
         const program = await strapi.services.programs.findOne({ id:data.program_id });   
         const money_id = data.payuMoneyId
@@ -56,29 +47,16 @@ module.exports = {
         const programEnrollment = {}
         programEnrollment.student = sanitizedStudentEntity
         programEnrollment.institution = institution
-        programEnrollment.batch = program.default_enrollment_batch
+        programEnrollment?.batch = program.default_enrollment_batch
         programEnrollment.status = 'Enrollment Request Received' // default status is Enrolled
         programEnrollment.registration_date = new Date()
-        if(money_id == 0){
-           programEnrollment.fee_status ='Waived Off'
-        }   
-        else{
-           programEnrollment.fee_status='Paid'
-        }
-        // programEnrollment.fee_status = NOT_FOUND
-        // programEnrollment.fee_payment_date = data.addedon
+        programEnrollment.fee_status = money_id == 0 ? 'Waived Off' : 'Paid'
         programEnrollment.fee_amount = data.amount
         programEnrollment.fee_transaction_id = data.fee_transaction_id
-        // programEnrollment.fee_refund_status = NOT_FOUND
-        // programEnrollment.fee_refund_date = NOT_FOUND
         programEnrollment.course_type = data.course_type
         programEnrollment.course_level = data.course_level
         programEnrollment.course_year = data.course_year
-        // programEnrollment.year_of_course_completion = NOT_FOUND
-        // programEnrollment.certification_date = NOT_FOUND
-        // programEnrollment.institution_name_entered_by_student = NOT_FOUND
-        // programEnrollment.medha_program_certificate = NOT_FOUND
-        // programEnrollment.course_name_in_current_sis = NOT_FOUND
+
         let programEnrollmentEntity = await strapi.services['program-enrollments'].create(programEnrollment)
         let sanitizedProgramEnrollmentEntity = sanitizeEntity(programEnrollmentEntity, { model: strapi.models['program-enrollments'] })
         console.log(`
