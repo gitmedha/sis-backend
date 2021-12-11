7b1d652cce8f883f04c602cd87768c0db4b9bfdf
Swapneel Rane <swapneelrane@gmail.com>
2021-12-11 22:43:53 +0530

    add create & upddate code to surveys & attendance

--- api/surveys/controllers/surveys.js
+++ api/surveys/controllers/surveys.js
@@ -1,8 +1,29 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;    
+    logged_in_user = ctx.state.user.id;
+    data = ctx.request.body;      
+    data.created_by_frontend = logged_in_user;
+    data.updated_by_frontend = logged_in_user;
+    entity = await strapi.services.surveys.create(data);
+    return sanitizeEntity(entity, { model: strapi.models.surveys});
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+    let entity;
+    logged_in_user = ctx.state.user.id;
+    data = ctx.request.body;
+    data.updated_by_frontend = logged_in_user;
+    entity = await strapi.services.surveys.update({ id }, data);
+    return sanitizeEntity(entity, { model: strapi.models.surveys });
+  },
+};
