9250f1b51422939861ea92b08746d7ab26bae37c
Swapneel Rane <swapneelrane@gmail.com>
2021-12-12 20:56:20 +0530

    modify students, institutions and opportunities

--- api/students/controllers/students.js
+++ api/students/controllers/students.js
@@ -7,6 +7,28 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
  */
 
 module.exports = {
+
+    async create(ctx) {
+        let entity;    
+        logged_in_user = ctx.state.user.id;
+        data = ctx.request.body;
+        data.assigned_to = data.assigned_to == null ? logged_in_user : data.assigned_to;
+        data.created_by_frontend = logged_in_user;
+        data.updated_by_frontend = logged_in_user;
+        entity = await strapi.services.students.create(data);
+        return sanitizeEntity(entity, { model: strapi.models.students});
+      },
+    
+      async update(ctx) {
+        const { id } = ctx.params;
+        let entity;
+        logged_in_user = ctx.state.user.id;
+        data = ctx.request.body;
+        data.updated_by_frontend = logged_in_user;
+        entity = await strapi.services.students.update({ id }, data);
+        return sanitizeEntity(entity, { model: strapi.models.students });
+      },
+
     // create from webhook
     async createFromWebhook (ctx) {
         const data = ctx.request.body
@@ -74,5 +96,22 @@ module.exports = {
 
         ctx.send(sanitizedStudentEntity)
         return sanitizedStudentEntity
-    }
+    },
+
+    async delete(ctx) {
+        const { id } = ctx.params;
+        const record = await strapi.services.students.findOne({ id });
+        if (!record.assigned_to) {
+            ctx.throw(401, 'This record is not assigned to any user!');
+        } else if ( 
+            (ctx.state.user.role.name == "Basic" && record.assigned_to.id == ctx.state.user.id) ||
+            (ctx.state.user.role.name == "Advanced" && record.medha_area == ctx.state.user.area) ||
+            ctx.state.user.role.name == "Admin"
+        ) {
+            const entity = await strapi.services.students.delete({ id });
+            return sanitizeEntity(entity, { model: strapi.models.students });
+        } else {
+            ctx.throw(401, 'You are not allowed to delete this record!', { user: ctx.state.user.username});
+        }
+      },
 };
