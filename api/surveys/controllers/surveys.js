821223676aeb2e9d514e8c6fbb3b452448d2d80b
Swapneel Rane <swapneelrane@gmail.com>
2021-12-12 20:48:15 +0530

    add custom controller to employers

--- api/surveys/controllers/surveys.js
+++ api/surveys/controllers/surveys.js
@@ -41,4 +41,31 @@ module.exports = {
         ctx.throw(401, 'You are not allowed to delete this record!', { user: ctx.state.user.username});
     }
   },
+
+  // CREATE FROM WEBHOOK
+  /*
+  async createFromWebhook (ctx) {
+    console.log('ctx.request.body: ',  ctx.request.body)
+    const employer = {}
+    employer.name = ctx.request.body.name
+    let entity = await strapi.services.employers.create(employer)
+    let sanitizedEntity = sanitizeEntity(entity, { model: strapi.models.employers })
+    console.log(`
+        Employer Created! ID: ${sanitizedEntity.id}
+        NAME: ${sanitizedEntity.name}
+        CREATED AT: ${sanitizedEntity.created_at}
+        `)
+    const data = ctx.request.body        
+    Object.keys(data).forEach(async key => {
+        const surveyResponse = {}
+        surveyResponse.survey_response_id = sanitizedEntity.id
+        surveyResponse.question = key
+        surveyResponse.response = data[key]
+        let unpivotedSurveyResponse = await strapi.services.surveys.create(surveyResponse)
+        console.log("unpivotedSurveyResponse: ", unpivotedSurveyResponse)
+    })
+    ctx.send(sanitizedEntity)
+    return sanitizedEntity
+  },
+  */
 };
