3bc9b2a2cc62c73f15977f031093a1ff67745fa0
Swapneel Rane <swapneelrane@gmail.com>
2021-12-12 21:12:55 +0530

    modify empl_conn and prog_enrl

--- api/program-enrollments/controllers/program-enrollments.js
+++ api/program-enrollments/controllers/program-enrollments.js
@@ -1,8 +1,41 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
 
-module.exports = {};
+  async create(ctx) {
+    let entity;    
+    logged_in_user = ctx.state.user.id;
+    data = ctx.request.body;
+    data.created_by_frontend = logged_in_user;
+    data.updated_by_frontend = logged_in_user;
+    entity = await strapi.services['program-enrollments'].create(data);
+    return sanitizeEntity(entity, { model: strapi.models['program-enrollments']});
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+    let entity;
+    logged_in_user = ctx.state.user.id;
+    data = ctx.request.body;
+    data.updated_by_frontend = logged_in_user;
+    entity = await strapi.services['program-enrollments'].update({ id }, data);
+    return sanitizeEntity(entity, { model: strapi.models['program-enrollments'] });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+    const record = await strapi.services['program-enrollments'].findOne({ id });
+    if (!record.student.assigned_to) {
+        ctx.throw(401, 'This student is not assigned to any user!');
+    } else if ( 
+        (ctx.state.user.role.name == "Basic" && record.student.assigned_to.id == ctx.state.user.id) ||
+        (ctx.state.user.role.name == "Advanced" && record.student.medha_area == ctx.state.user.area) ||
+        ctx.state.user.role.name == "Admin"
+    ) {
+        const entity = await strapi.services['program-enrollments'].delete({ id });
+        return sanitizeEntity(entity, { model: strapi.models['program-enrollments'] });
+    } else {
+        ctx.throw(401, 'You are not allowed to delete this record!', { user: ctx.state.user.username});
+    }
+  },
+};
