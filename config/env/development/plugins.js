f3441dcd7cfda4cf595b3003f630c9313846afcf
Swapneel Rane <swapneelrane@gmail.com>
2021-12-26 12:39:09 +0530

    replace upload provider plugin and config

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -1,25 +1,27 @@
 module.exports = ({ env }) => ({
 
-    upload: {
-      provider: 'azure-storage-blob',
-      providerOptions: {
-        account: env('STORAGE_ACCOUNT', 'medhasisfiles'),
-        serviceBaseURL: env('STORAGE_URL', 'https://medhasisfiles.blob.core.windows.net/staging-server-files'),
-        containerName: env('STORAGE_CONTAINER_NAME', 'staging-server-files'),
-        connectionString: env('STORAGE_CONNECTION_STRING', 'DefaultEndpointsProtocol=https;AccountName=medhasisfiles;AccountKey=2HM6xrhwEOmm8ciRRIB8f2VjZ62DoXbR03mRgZWblHmnt0UzNnrVNTthG3+UpHWazw1EmdLPX5HkvGAyBvb44A==;EndpointSuffix=core.windows.net'),
-        defaultPath: 'assets',
-      }
-    },
-
-    graphql: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: true,
-      },
+  upload: {
+    provider: 'azure-storage',
+    providerOptions: {
+      account: env('STORAGE_ACCOUNT', 'medhasisfiles'),
+      accountKey: env('STORAGE_ACCOUNT_KEY', '2HM6xrhwEOmm8ciRRIB8f2VjZ62DoXbR03mRgZWblHmnt0UzNnrVNTthG3+UpHWazw1EmdLPX5HkvGAyBvb44A=='),
+      serviceBaseURL: env('STORAGE_URL', 'https://medhasisfiles.blob.core.windows.net/'),
+      containerName: env('STORAGE_CONTAINER_NAME', 'staging-server-files'),
+      // cdnBaseURL: env('STORAGE_CDN_URL'),
+      defaultPath: 'development-files',
+      maxConcurrent: 10
     }
+  },
 
-  });
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: true,
+    },
+  }
+
+});
