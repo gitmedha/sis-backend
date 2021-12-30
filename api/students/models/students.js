91120ffd6165f3f354a3e01b8829526cf25ce8fb
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-12-30 20:18:03 +0530

    deleting program enrollment and employment connection for student

--- api/students/models/students.js
+++ api/students/models/students.js
@@ -5,4 +5,17 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    // Called after an entry is deleted
+    async beforeDelete(params) {
+      const { id } = params;
+
+      // find program enrollments for the student and delete them
+      await strapi.query('program-enrollments').delete({ student: id })
+
+      // find employment connections for the student and delete them
+      await strapi.query('employment-connections').delete({ student: id })
+    },
+  },
+};
