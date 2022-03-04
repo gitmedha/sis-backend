3bea4b13e1c03e03651e9b21bd41cb6de7d7401f
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-03-04 22:33:18 +0530

    program enrollment processing only if percentage above 75

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -13,9 +13,17 @@
 module.exports = {
   '* * * * *': async () => {
     const programEnrollments = await strapi.services['program-enrollments'].find({ medha_program_certificate_status: 'processing', _limit: 4 });
+    console.log('cron processing entries:', programEnrollments.length);
     programEnrollments.forEach(async programEnrollment => {
+      console.log('processing program enrollment:', programEnrollment.id);
       // generate certificate for program enrollment
-      await strapi.services['program-enrollments'].generateCertificate(programEnrollment);
+      let batchAttendancePercent = await strapi.services['program-enrollments'].calculateBatchAttendance(programEnrollment);
+      console.log('batchAttendancePercent found:', batchAttendancePercent);
+      if (batchAttendancePercent >= 75) {
+        await strapi.services['program-enrollments'].generateCertificate(programEnrollment);
+      } else {
+        await strapi.services['program-enrollments'].update({ id: programEnrollment.id }, { medha_program_certificate_status: 'low-attendance' });
+      }
     });
   }
 };
