4a2ebbb968003ba6213217f01d70557f09712b86
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-06-10 13:56:59 +0530

    updating program enrollment status on batch status update

--- api/batches/controllers/batches.js
+++ api/batches/controllers/batches.js
@@ -20,6 +20,11 @@ module.exports = {
     data = ctx.request.body;
     data.updated_by_frontend = logged_in_user;
     entity = await strapi.services.batches.update({ id }, data);
+
+    if (data.status === 'Certified') {
+      await strapi.services['batches'].handleProgramEnrollmentOnCertification(entity);
+    }
+
     return sanitizeEntity(entity, { model: strapi.models.batches });
   },
 
