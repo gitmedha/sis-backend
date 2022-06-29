642704898e8b455a732017f642fded88c82b0ff2
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-06-29 13:03:28 +0530

    cron correction for multiple jobs

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -12,27 +12,34 @@
 
 module.exports = {
   '* * * * *': async () => {
-    const programEnrollments = await strapi.services['program-enrollments'].find({ medha_program_certificate_status: 'processing', _limit: 4 });
-    programEnrollments.forEach(async programEnrollment => {
-      // create the certificate
-      await strapi.services['program-enrollments'].generateCertificate(programEnrollment);
-    });
-  },
-  '* * * * *': async () => {
-    // cron to parse all students and fill registration_date_latest for them
-    let studentIds = await strapi.connections.default.raw(
-      `select DISTINCT(program_enrollments.student) from program_enrollments
-      join students on students.id = program_enrollments.student
-      where students.registration_date_latest is null
-      order by program_enrollments.student
-      limit 1000`
-    );
-    studentIds = JSON.stringify(studentIds.rows);
-    studentIds = JSON.parse(studentIds).map((a) => a.student);
-    console.log('studentIds', studentIds);
-    await studentIds.forEach(async studentId => {
-      let student = await strapi.services['students'].findOne({id: studentId});
-      await strapi.services['students'].handleRegistrationDateLatest(student);
-    });
+    await generateProgramEnrollmentCertificates();
+    await processRegistrationDateLatestForStudents();
   },
 };
+
+const generateProgramEnrollmentCertificates = async () => {
+  const programEnrollments = await strapi.services['program-enrollments'].find({ medha_program_certificate_status: 'processing', _limit: 4 });
+  console.log('programEnrollments', programEnrollments.length);
+  programEnrollments.forEach(async programEnrollment => {
+    // create the certificate
+    await strapi.services['program-enrollments'].generateCertificate(programEnrollment);
+  });
+}
+
+const processRegistrationDateLatestForStudents = async () => {
+  // cron to parse all students and fill registration_date_latest for them
+  let studentIds = await strapi.connections.default.raw(
+    `select DISTINCT(program_enrollments.student) from program_enrollments
+    join students on students.id = program_enrollments.student
+    where students.registration_date_latest is null
+    order by program_enrollments.student
+    limit 1000`
+  );
+  studentIds = JSON.stringify(studentIds.rows);
+  studentIds = JSON.parse(studentIds).map((a) => a.student);
+  console.log('studentIds', studentIds.length);
+  await studentIds.forEach(async studentId => {
+    let student = await strapi.services['students'].findOne({id: studentId});
+    await strapi.services['students'].handleRegistrationDateLatest(student);
+  });
+}
