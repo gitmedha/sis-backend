e08714226dbbef9d02ef083eef86ef1f638adc60
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-06-30 18:54:51 +0530

    sessions update/create controller functions with audit entries

--- api/sessions/controllers/sessions.js
+++ api/sessions/controllers/sessions.js
@@ -5,4 +5,30 @@
  * to customize this controller
  */
 
-module.exports = {};
+ module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    logged_in_user = ctx.state.user.id;
+    data = ctx.request.body;
+    data.created_by_frontend = logged_in_user;
+    data.updated_by_frontend = logged_in_user;
+    entity = await strapi.services.sessions.create(data);
+    return sanitizeEntity(entity, { model: strapi.models.sessions});
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+    let entity;
+    logged_in_user = ctx.state.user.id;
+    data = ctx.request.body;
+    data.updated_by_frontend = logged_in_user;
+    entity = await strapi.services.sessions.update({ id }, data);
+    return sanitizeEntity(entity, { model: strapi.models.sessions });
+  },
+};
