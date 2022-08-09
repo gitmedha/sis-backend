2cda3c3ebfbc231345fb45be7078f799ee30169b
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-08-10 00:15:32 +0530

    bulk update registered by for students

--- api/students/services/students.js
+++ api/students/services/students.js
@@ -17,5 +17,24 @@ module.exports = {
   },
   async getLatestProgramEnrollment(student) {
     return await strapi.services['program-enrollments'].findOne({ student: student.id, _sort: 'id:desc' });
+  },
+  async bulkUpdateRegisteredBy() {
+    let students = await strapi.services['students'].find({ registered_by_null: true, _limit: 1000 });
+    if (students.length) {
+      console.log('total students to process: ', students.length);
+      for (const student of students) {
+        console.log('processing student: ', student.id);
+        let studentProgramEnrollment = await strapi.services['program-enrollments'].findOne({
+          student: student.id,
+          institution_null: false,
+          _sort: 'created_at:asc'
+        });
+        if (studentProgramEnrollment) {
+          await strapi.services['students'].update({ id: student.id }, {
+            registered_by: studentProgramEnrollment.institution.assigned_to
+          });
+        }
+      }
+    }
   }
 };
