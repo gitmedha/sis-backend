3c37f1f34e889bf9bd908a32a451bbced1f6ad8e
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-08-23 16:45:05 +0530

    delete attendance if batch is chagned for pe non-certified/non-complete

--- api/program-enrollments/models/program-enrollments.js
+++ api/program-enrollments/models/program-enrollments.js
@@ -11,6 +11,19 @@ module.exports = {
     async afterCreate(result, data) {
       await strapi.services['students'].handleRegistrationDateLatest( result.student);
     },
+    // Called before an entry is updated
+    async beforeUpdate(params, data) {
+      console.log('beforeUpdate.......');
+      const { id } = params
+      const programEnrollment = await strapi.services['program-enrollments'].findOne({ id })
+
+      if (programEnrollment.batch.id !== parseInt(data.batch) && programEnrollment.status !== ('Certified by Medha' || 'Batch Complete')) {
+        // batch is changed for the program enrollment where
+        // status is not certified or completed
+        // delete the attendance records for the program enrollment
+        await strapi.query('attendance').delete({ program_enrollment: programEnrollment.id });
+      }
+    },
     // Called after an entry is updated
     async afterUpdate(result, params, data) {
       await strapi.services['students'].handleRegistrationDateLatest( result.student);
