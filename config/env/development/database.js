220926254022d0e3c9ceddcec3f91c421a70b7ed
Deepak Sharma <deepak@aayworks.com>
2023-04-26 15:20:06 +0530

    revered the db name and password

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
           client: 'postgres',
           host: env('DATABASE_HOST', 'localhost'),
           port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'sis'),
+          database: env('DATABASE_NAME', 'medha_sis'),
           username: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', 'rohit'),
+          password: env('DATABASE_PASSWORD', 'postgres_sis'),
           ssl: env.bool('DATABASE_SSL', false),
         },
         options: {}
