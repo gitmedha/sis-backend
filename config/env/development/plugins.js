ca37a002db4610ea682af9a42cfcea93a241639d
rohit-sharma-medha <115704971+rohit-sharma-medha@users.noreply.github.com>
2023-05-31 12:03:23 +0530

    Update plugins.js
    Changes in plugin for aws-s3

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -36,16 +36,15 @@ module.exports = ({ env }) => ({
   },
 
   upload: {
-    provider: 'azure-storage',
-    providerOptions: {
-      account: env('STORAGE_ACCOUNT', 'medhasisfiles'),
-      accountKey: env('STORAGE_ACCOUNT_KEY', '2HM6xrhwEOmm8ciRRIB8f2VjZ62DoXbR03mRgZWblHmnt0UzNnrVNTthG3+UpHWazw1EmdLPX5HkvGAyBvb44A=='),
-      serviceBaseURL: env('STORAGE_URL', 'https://medhasisfiles.blob.core.windows.net/'),
-      containerName: env('STORAGE_CONTAINER_NAME', 'staging-server-files'),
-      // cdnBaseURL: env('STORAGE_CDN_URL'),
-      defaultPath: 'development-files',
-      maxConcurrent: 10
-    }
+  provider: 'aws-s3',
+  providerOptions: {
+    accessKeyId: 'AKIAYKQAZ2TY2FND7R6N',
+    secretAccessKey: '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',
+    region: 'ap-south-1',
+    params: {
+      Bucket: 'medhasisstg',
+    },
+   },
   },
 });
 
