84ab4912dec205a1be832ec658c4338dac9c9b10
Rohit <rohitk7065@gmail.com>
2023-06-06 13:07:42 +0530

    removed aws cofig

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -37,14 +37,7 @@ module.exports = ({ env }) => ({
 
   upload: {
   provider: 'aws-s3',
-  providerOptions: {
-    accessKeyId: 'AKIAYKQAZ2TY2FND7R6N',
-    secretAccessKey: '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',
-    region: 'ap-south-1',
-    params: {
-      Bucket: 'medhasisstg',
-    },
-   },
+ 
   },
 });
 
