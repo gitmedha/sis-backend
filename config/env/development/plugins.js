3cdeed4fe690b327e974c53a44ed77b20ee70def
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-06-13 13:51:20 +0530

    plugins update for s3

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -36,8 +36,15 @@ module.exports = ({ env }) => ({
   },
 
   upload: {
-  provider: 'aws-s3',
- 
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('S3_ACCESS_KEY_ID'),
+      secretAccessKey: env('S3_SECRET_KEY'),
+      region: env('S3_REGION'),
+      params: {
+        Bucket: env('S3_BUCKET'),
+      },
+    },
   },
 });
 
