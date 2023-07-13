0b51cfdae04745823f3e3339ed7fbcf3010ece73
Deepak Sharma <deepak@aayworks.com>
2023-07-13 12:41:16 +0530

    added a new api

--- api/users-ops-activities/controllers/users-ops-activities.js
+++ api/users-ops-activities/controllers/users-ops-activities.js
@@ -5,4 +5,18 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async createMany(ctx) {
+        const { request } = ctx;
+        const { body } = request;
+    
+        try {
+          // Assuming your model name is "mydata"
+          const createdData = await strapi.services.mydata.createMany(body);
+          return createdData;
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
+      },
+};
