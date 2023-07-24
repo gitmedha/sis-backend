67b9c9664532389e011893b00d8ece09665b8d45
Deepak Sharma <deepak@aayworks.com>
2023-07-24 16:28:22 +0530

    bulk create for users_tots

--- api/users-tot/services/users-tot.js
+++ api/users-tot/services/users-tot.js
@@ -5,4 +5,14 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+    createMany: async (data) => {
+        try {
+          const createdData = await strapi.query('users-tot').createMany(data);
+          return createdData;
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
+      },
+};
