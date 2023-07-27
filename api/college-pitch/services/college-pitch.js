47347b05f1a8e9a787d1873ed66c3d0863a91346
Deepak Sharma <deepak@aayworks.com>
2023-07-27 14:30:30 +0530

    added college pitch collection type and bulk create feature

---
+++ api/college-pitch/services/college-pitch.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
+ * to customize this service
+ */
+
+
+module.exports = {
+    createMany: async (data) => {
+        try {
+          const createdData = await strapi.query('college-pitch').createMany(data);
+          return createdData;
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
+      },
+};
+
