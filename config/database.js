dc7841f2b3c651b00fc5d075f225175daa374cbf
srivastava-jyoti <jyoti.srivastava@coloredcow.in>
2024-05-09 15:08:31 +0530

    cleanups

--- config/database.js
+++ config/database.js
@@ -6,10 +6,10 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'postgres',
         host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5433),
+        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'medha_sis'),
         username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'postgres_sis'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
