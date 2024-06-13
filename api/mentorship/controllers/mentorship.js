3f8463d4b345c36933ae1ef33b6341b409a7b53d
rohit-shamra-medha <rohit.sharma@medha.org.in>
2024-06-13 23:55:06 +0530

    Added mentorship backend

---
+++ api/mentorship/controllers/mentorship.js
@@ -0,0 +1,134 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    async createBulkmentorship(ctx) {
+        const { body } = ctx.request;
+        
+        try {
+          const createdData = await strapi.services['mentorship'].createMany(body);
+          return createdData;
+       
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
+      },
+      // async searchOps(ctx) {
+      //   const { searchField, searchValue } = ctx.request.body;
+      
+      //   try {
+      //     if (!searchField || !searchValue) {
+      //       return ctx.badRequest('Field and value are required.');
+      //     }
+          
+      //     if(searchValue.hasOwnProperty('start_date')){
+
+      //       const records = await strapi.query('students-upskilling').find({
+      //         'start_date_gte': searchValue.start_date,
+      //         'end_date_lte': searchValue.end_date,
+      //         isactive:true,
+      //         _limit: 1000000,
+      //         _start: 0,
+      //         _sort:`${searchField}:asc`
+      //       });
+            
+      
+      //       return ctx.send(records);
+
+           
+      //     }
+      //     else {
+      //       const records = await strapi.query('students-upskilling').find({
+      //         [`${searchField}_contains`]: searchValue,
+      //         isactive:true,
+      //         _limit:1000000,
+      //         _start: 0
+      //       });
+            
+  
+      //       return ctx.send(records);
+      //     }
+         
+      //   } catch (error) {
+      //     console.log(error);
+      //     throw error;
+      //   }
+      // },
+      async findDistinctField(ctx) {
+        const { field } = ctx.params; // Extract the field name from the query parameters
+        let optionsArray = [];
+      
+        try {
+          if (field === 'program_name') {
+            const programs = await strapi.query('programs').find({
+              _start:0,
+            })
+    
+      
+          for (let row = 0; row < programs.length; row++) {
+            let valueToAdd;
+            valueToAdd = programs[row]['name'];
+    
+            optionsArray.push({
+              key: row,
+              label: valueToAdd,
+              value: valueToAdd,
+            });
+          }
+  
+      
+          return ctx.send(optionsArray);
+          }
+          else {
+            
+              const values = await strapi.query('students-upskilling').find({
+                isactive:true,
+                _limit: 100,
+                _start: 0,
+              });
+            
+              const uniqueValuesSet = new Set();
+          
+              for (let row = 0; row < values.length; row++) {
+                let valueToAdd;
+          
+                if (field === "student_id") {
+                  valueToAdd = values[row][field].full_name;
+    
+                }
+                else if (field === "assigned_to"){
+                  valueToAdd = values[row][field].username;
+                }
+                else if (field === "institution"){
+                  valueToAdd = values[row][field].name;
+                }
+                
+                else if (field) {
+                  valueToAdd = values[row][field];
+                }
+          
+                if (!uniqueValuesSet.has(valueToAdd)) {
+                  optionsArray.push({
+                    key: row,
+                    label: valueToAdd,
+                    value: valueToAdd,
+                  });
+                  uniqueValuesSet.add(valueToAdd);
+                }
+              }
+          
+              return ctx.send(optionsArray); 
+
+          }
+        
+        } catch (error) {
+        
+          return ctx.badRequest('An error occurred while fetching distinct values.');
+        }
+      }
+};
