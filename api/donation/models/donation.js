4febe6d904768d51e498bcd86a2d7243832b6839
Rohit-Sharma-Dev <rohitshharmma@gmail.com>
2024-09-09 13:21:42 +0530

    Added donation table

---
+++ api/donation/models/donation.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {
+
+    lifecycles: {
+        async beforeCreate(data) {
+          if (!data.student_id || !data.donation_id || !data.amount || !data.status) {
+            throw new Error('Required fields are missing');
+          }
+        },
+      },
+};
