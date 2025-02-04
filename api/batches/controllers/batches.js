b9933e060ac996ec72fcb6012e864b0b915f5535
deepaksharma-dev <deepak.sharma@medha.org.in>
2025-02-04 15:44:12 +0530

    added manual reminder and template changes

--- api/batches/controllers/batches.js
+++ api/batches/controllers/batches.js
@@ -1,4 +1,6 @@
 const { sanitizeEntity } = require("strapi-utils");
+const moment = require('moment');
+
 
 module.exports = {
   async create(ctx) {
@@ -305,7 +307,62 @@ module.exports = {
       console.log("Error in sendEmailOnCreationAndCompletion:", error);
       return ctx.badRequest(error.message);
     }
-  }
+  },
+  async sendReminderEmail (ctx){
+    try{
+      const { id } = ctx.params;
+      console.log("id",id);
+        const batches = await strapi.services['batches'].find({ id:id});
+    
+        for (const batch of batches) {
+            const { last_attendance_date, id, name } = batch;
+    
+            const assignedTo = await strapi.plugins['users-permissions'].services.user.fetch({
+                id: Number(batch.assigned_to.id),
+            });
+    
+            const srmName = assignedTo.username;
+            const srmEmail = assignedTo.email;
+            const managerEmail = assignedTo.reports_to?.email;
+             // Generate the dynamic link
+             const baseUrl = 'https://sisstg.medha.org.in';
+             const attendanceLink = `${baseUrl}/batch/${id}`;
+ 
+             // Trigger email
+             await strapi.plugins['email'].services.email.send({
+                 to:'deepak.sharma@medha.org.in',
+                 cc: [managerEmail, 'kirti.gour@medha.org.in','maryam.raza@medha.org.in','sanskaar.pradhan@medha.org.in'],
+                 subject: `Reminder: Mark Attendance for Batch ${name}`,
+                 text: `
+                     Dear ${srmName},
+                     
+                     This is a reminder that attendance for batch "${name}" has not been updated since ${moment(last_attendance_date).format('MMMM DD, YYYY')}.
+                     Please ensure it is marked within the next 2 days to maintain accurate records.
+                     
+                     You can update the attendance by clicking on the following link: [Mark Attendance Now](${attendanceLink})
+                     
+                     Best,
+                     Data Management
+                 `,
+                 html: `
+                     <p>Dear ${srmName},</p>
+                     <p>
+                         This is a reminder that attendance for batch "<strong>${name}</strong>" has not been updated since ${moment(last_attendance_date).format('MMMM DD, YYYY')}.
+                         Please ensure it is marked within the next 2 days to maintain accurate records.
+                     </p>
+                     <p>
+                         You can update the attendance by clicking on the following link: <a href="${attendanceLink}" target="_blank">Mark Attendance Now</a>
+                     </p>
+                     <p>Best,<br>Data Management</p>
+                 `,
+             });
+    
+        }
+        return ctx.send("successfully! reminder sent");
 
+      }catch(e){
+        console.log('Error in cron job', e);
+      }   
+  }
 
 };
