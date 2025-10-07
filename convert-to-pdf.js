1a4857bf7c02a95d0c2772eb5e923f032d89d5c0
deepaksharma-dev <deepak.sharma@medha.org.in>
2025-10-07 10:44:29 +0530

    added otg certificate changes

--- convert-to-pdf.js
+++ convert-to-pdf.js
@@ -7,13 +7,24 @@ const path = require('path'); // Import the path module to handle file paths
     const page = await browser.newPage();
     
     // Construct the absolute file path
-    const filePath = path.resolve(__dirname, './public/program-enrollment-certificate-template/pehliUdaan/certificate.html');
+    const filePath = path.resolve(__dirname, './public/program-enrollment-certificate-template/otg/certificate.html');
     
     // Use the file:// protocol with the absolute path
     await page.goto(`file://${filePath}`, { waitUntil: 'networkidle0' });
 
     // Generate PDF
-    await page.pdf({ path: 'output.pdf', format: 'A4', printBackground: true });
+    await page.pdf({
+      path: 'output.pdf',
+      width: '1440px',
+      height: '1120px',
+      printBackground: true,
+      margin: {
+        left: '0px',
+        top: '0px',
+        right: '0px',
+        bottom: '0px'
+      }
+    });
 
     await browser.close();
     console.log('PDF successfully created!');
