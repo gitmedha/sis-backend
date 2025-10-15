cb771ef61828780cacafe23b33fe1894f62e745b
deepaksharma-dev <deepak.sharma@medha.org.in>
2025-10-15 15:26:31 +0530

    added new changes

--- convert-to-pdf.js
+++ convert-to-pdf.js
@@ -8,8 +8,7 @@ const path = require('path'); // Import the path module to handle file paths
     
     // Construct the absolute file path
 
-    const filePath = path.resolve(__dirname, './public/program-enrollment-certificate-template/EmplifyWithAi/certificate.html');
-    // /home/rohit/Desktop/codebase/medha-backend-strapi/public/program-enrollment-certificate-template/EmplifyWithAi/certificate.html
+    const filePath = path.resolve(__dirname, './public/program-enrollment-certificate-template/otg/certificate.html');
 
     
     // Use the file:// protocol with the absolute path
